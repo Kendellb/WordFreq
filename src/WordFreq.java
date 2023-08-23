1aaca9c23f35acfa2a0b33e3c3780f3031dcecd8
Kendellb <92357397+Kendellb@users.noreply.github.com>
2023-08-23 11:52:12 -0400

    sorts from smallest to largest and added a output.txt and cleaned up the code

--- src/WordFreq.java
+++ src/WordFreq.java
@@ -10,33 +10,15 @@ public class WordFreq {
 
 
 
-    // Loop to iterate over the words
-        for(int i=0;i<arr.length;i++){
-        // Condition to check if the
-        // array element is present
-        // the hash-map
-        if(mp.containsKey(arr[i])) {
-            mp.put(arr[i], mp.get(arr[i])+1);
-        }
-        else {
-            mp.put(arr[i],1);
-        }
-    }
-
-         /*ArrayList<Integer> list = new ArrayList<>();
-         for (Map.Entry<String, Integer> entry : mp.entrySet()) {
-             list.add(entry.getValue());
+         for (String s : arr) {
+             if ( mp.containsKey(s) ) {
+                 mp.put(s, mp.get(s) + 1);
+             } else {
+                 mp.put(s, 1);
+             }
          }
 
-         Collections.sort(list);
-         for (int num : list) {
-             for (Map.Entry<String, Integer> entry : mp.entrySet()) {
-                 if (entry.getValue().equals(num)) {
-                     sortedMap.put(entry.getKey(), num);
-                 }
-             }
-         }*/
-         //sortedMap = (LinkedHashMap<String, Integer>) MapUtil.sortByValue(mp);
+         //from geeksforgeeks
          sortedMap = mp.entrySet()
                  .stream()
                  .sorted(Map.Entry.comparingByValue())
@@ -45,48 +27,42 @@ public class WordFreq {
                          Map.Entry::getValue,
                          (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
-    // Loop to iterate over the
-    // elements of the map
-    /*    for(Map.Entry<String,Integer> entry:
-            mp.entrySet())
-    {
-        System.out.println(entry.getKey()+
-                " - "+entry.getValue());
-    }
-     */
 }
 
     public static void main(String[] args) throws IOException {
-        //insert file
-        //run method
-        //print output
         WordFreq wf = new WordFreq();
 
-        FileReader file = new FileReader("/home/kendell/Documents/ComputerNetworks/Word-Frequency/Word-Frequency/src/example.txt");
+        FileReader file = new FileReader("/home/kendell/Documents/ComputerNetworks/Word-Frequency/Word-Frequency/src/TheHoundOfTheBaskervilles.txt");
         BufferedReader br = new BufferedReader(file);
 
         String line;
-        int overAllCount = 0;
+        double overAllCount = 0;
         while ((line = br.readLine()) != null) {
             String[] words = line.split(" ");
             for(int i = 0; i< words.length;i++){
                 words[i] = words[i].toLowerCase();
                 words[i] = words[i].replaceAll("[^\\sa-zA-Z0-9]", "");
                 words[i] = words[i].replaceAll("\\p{Punct}", "");
+
             }
             overAllCount = overAllCount + words.length;
             wf.count_freq(words);
-            //System.out.println(Arrays.toString(words));
-            //split into lines
-            //put the words into an array
-            //using a HashMap to count the frequency of the words.
-            //get the count of the whole text to get the percentage of the usage
         }
-        //System.out.println(sortedMap);
-        for (Map.Entry<String, Integer> en : sortedMap.entrySet()) {
-            System.out.println("Word:  " + en.getKey() +
-                    "\n Freq.:  " + en.getValue() + "\n Percentage:  " + en.getValue()/overAllCount);
+
+        double percentage = 0;
+        Set<String> set = sortedMap.keySet();
+
+        List<String> alKeys = new ArrayList<String> (sortedMap.keySet());
+
+        Collections.reverse(alKeys);
+
+        for (String strKey : alKeys) {
+            percentage = (sortedMap.get(strKey)/overAllCount) * 100;
+            String result = String.format("%.2f", percentage);
+
+            System.out.println("Word: " + strKey +
+                    "  Freq.:  " + sortedMap.get(strKey) + "  Percentage:  " + result + "%" );
         }
-        //System.out.println("Word Count: " + overAllCount);
+
     }
 }
